b655f1974e7850af9defefd5ee2d524f68a3be24
ThaliumFr <theomaxmi34@gmail.com>
2023-12-23 18:52:34 +0100

    end

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ document.addEventListener('scroll', (e) => {
 })
 
 document.addEventListener('DOMContentLoaded', (e) => {
+    console.log('DOM loaded');
     parallaxElements.forEach(element => {
         const parralaxPower = element.dataset.parallax;
         let scrollY = window.scrollY*parralaxPower;
         element.style.backgroundPosition = "center "+scrollY+"px";
     });
+    console.log('Parallax initialized');
 
     nav.classList.toggle("is-pinned", scrollY != 0);
 })
